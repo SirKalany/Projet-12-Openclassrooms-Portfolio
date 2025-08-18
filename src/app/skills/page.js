ce1ecb33184608cc17e06a36f633eb3610ca603c
Kalany <duncan.miard@outlook.fr>
2025-08-18 10:03:00 +0200

    Structuration projet

---
+++ src/app/skills/page.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Skills() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-800">
+      <h1 className="text-3xl font-bold">Mes compétences 💻</h1>
+    </main>
+  );
+}
