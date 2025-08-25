821e2d11c76297fdb7bac63981b7392914d892e5
Kalany <duncan.miard@outlook.fr>
2025-08-25 07:26:53 +0200

    Modifications pour GitHub pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true, // nécessaire pour GitHub Pages
+};
 
 export default nextConfig;
